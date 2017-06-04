2a9aee88a4add14996eec3d394a528ffa3118476
Igor Laborie <ilaborie@gmail.com>
2017-06-04 13:01:11 +0200

    sol

--- plop/src/lazy.ts
+++ plop/src/lazy.ts
@@ -1,5 +1,9 @@
 import { execSync } from 'child_process';
 
+console.log('-----------------------------------');
+console.log('|         lazy parameter          |');
+console.log('-----------------------------------');
+
 const longComputationFunction = (): number => {
     // 😱 do not read the next line
     execSync('sleep 2');
@@ -32,3 +36,33 @@ const logger = new Logger();
 logger.info('Hello 🌍');
 logger.info(`❓: ${longComputationFunction()}`);
 logger.info('✅ Done');
+
+console.log('-----------------------------------');
+console.log('|         lazy attribute          |');
+console.log('-----------------------------------');
+
+
+type Supplier<T> = () => T;
+class lazy<T> {
+    private value: T | undefined = undefined;
+    constructor(private supplier: Supplier<T>) { }
+
+    get() {
+        if(this.value === undefined) {
+            this.value = this.supplier();
+            this.supplier = null;
+        }
+        return this.value;
+    }
+}
+
+var obj = {
+    lazyValue : new lazy(longComputationFunction)
+};
+
+console.log('call 1');
+console.log(obj.lazyValue.get());
+
+console.log('call 2');
+console.log(obj.lazyValue.get());
+
